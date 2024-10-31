ba24e43c2b3b2fe8588c3fab4968621c8bcda3db
akashdhadiwal <dhadiwalakash82@gmail.com>
2024-10-31 19:09:10 -0400

    lab 4 completed

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -26,9 +26,16 @@ export default function TOC() {
                   Lab 3
               </a>
           </li>
+          <li className="nav-item">
+              <a id="wd-a3" href="#/Labs/Lab4"
+                 className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}>
+                  Lab 4
+              </a>
+          </li>
           <li className="nav-item"><a id="wd-k" href="#/Kanbas" className="nav-link">Kanbas</a></li>
           <li className="nav-item">
-              <a id="wd-github" href="https://github.com/Akash-Dhadiwal/kanbas-react-web-app" target="_blank"
+              <a id="wd-github" href="https://github.com/Akash-Dhadiwal/kanbas-react-web-app"
+                 target="_blank"
                  className="nav-link" rel="noreferrer">
                   My GitHub
               </a>
